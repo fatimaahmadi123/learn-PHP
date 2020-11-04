d0484de96b961fc0f2f617f36bcb6206354eadf8
fatimaahmadi123 <73887840+fatimaahmadi123@users.noreply.github.com>
2020-11-04 13:22:03 +0200

    Add files via upload

---
+++ html.php
@@ -0,0 +1,24 @@
+<!DOCTYPE HTML>
+<html>
+<body>
+    <h1>  
+
+
+
+
+
+
+    <h2> 𝓕𝓪𝓬𝓮𝓫𝓸𝓸𝓴 </h2>
+    <form>
+        <label for='<fname>'> Username </label> <br>
+        <input type= 'text' id ='fname'  name="fname" value=""> <br>
+        <label for= '<Email>'> Email </label> <br>
+        <input type= '<text' id ='Email' name= 'Email' value=""> <br>
+        <label for="<password>"> Password </label> <br>
+        <input type="text and numburs" id= 'Password' name= 'Email' value="" > <br/> <br>
+        <input type="Submit" value="Submit"> 
+        
+    </form>
+
+</body>
+</html>
