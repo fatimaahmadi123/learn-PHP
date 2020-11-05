d74aaa5f2a3c8143dd004694723401e0b7ce2716
fatimaahmadi123 <73887840+fatimaahmadi123@users.noreply.github.com>
2020-11-05 13:08:38 +0200

    Add files via upload

---
+++ folder.php
@@ -0,0 +1,24 @@
+<html>
+ <head>
+     <title> Learning English </title>
+ </head>
+ <body>
+    <h1> members </h1>
+     <?php 
+    $file = "$member";
+    $member = " trim(fgets(member.txt));"
+    echo "<li>" .  $file  . "<li>"
+
+
+
+
+
+     
+     
+      ?>
+
+     
+ </body>
+
+</html>
+ 
